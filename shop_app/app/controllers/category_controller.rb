e605b855cbf7966f0379789f54494dca625701ed
TrangPhung <trang.phung@asiantech.vn>
2018-06-01 10:10:41 +0700

    commit master

---
+++ shop_app/app/controllers/category_controller.rb
@@ -0,0 +1,17 @@
+class CategoryController < ApplicationController
+ # protect_from_forgery with: :exception
+ def index
+   @categories = Category.all.where(parent_id: 0)
+   @category_child = Category.where(parent_id: params[:parent_id])
+   binding.pry
+ end
+
+  def new
+    @category = Category.new
+  end
+
+ private
+   def category_params
+     params.require(:category).permit(:name, :parent_id)
+   end
+end
