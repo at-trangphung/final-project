dc51b73468e7fa287e232e8041dc1549e4121cc4
TrangPhung <trang.phung@asiantech.vn>
2018-06-04 17:22:55 +0700

    create paginate home page

--- shop_app/app/controllers/shop_controller.rb
+++ shop_app/app/controllers/shop_controller.rb
@@ -2,7 +2,8 @@ class ShopController < BaseController
   before_action :logged_in?
   def index
     @categories = Category.all.where(parent_id: 0)
-    @productList = Product.all 
+    @productList = Product.all.paginate page: params[:page],
+            per_page: 9
   end
 
   private
