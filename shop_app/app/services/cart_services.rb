81642275d9a423bce410e3b33509bd6f5336239d
TrangPhung <trang.phung@asiantech.vn>
2018-06-19 08:37:44 +0700

    feature/manager products

--- shop_app/app/services/cart_services.rb
+++ shop_app/app/services/cart_services.rb
@@ -59,7 +59,6 @@
     def find_product_in_cart(product_id, size)
       return unless session[:shopping_cart]
       session[:shopping_cart].detect do |item|
-        binding.pry
         item["product_id"] == product_id
         item["size"] == size
       end
