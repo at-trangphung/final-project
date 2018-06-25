901de29a7e7cb12fbe8c686c530c22823c772d9c
TrangPhung <trang.phung@asiantech.vn>
2018-06-25 08:48:14 +0700

    admin/my comment

--- shop_app/config/routes.rb
+++ shop_app/config/routes.rb
@@ -43,6 +43,7 @@ Rails.application.routes.draw do
     resources :account_activations, only: :edit
     resources :password_resets, only: [:new, :create, :edit, :update]
     resources :customers
+    resources :my_comments
   end
 end
 
